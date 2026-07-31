4c940ad34b9ebd830a6474b045fac0a980446b83
Iván Maciel <ivanemanuelmaciel@gmail.com>
2026-07-31 00:02:15 -0300

    feat(friendships): trigger push notification on new friend request

--- src/modules/friendships/friendships.module.ts
+++ src/modules/friendships/friendships.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FriendshipsController } from './friendships.controller';
 import { FriendshipsService } from './friendships.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, NotificationsModule],
   controllers: [FriendshipsController],
   providers: [FriendshipsService]
 })
